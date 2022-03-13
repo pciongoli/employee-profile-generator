7d632e2288efe7e14320707e8d577e9a08fad993
pcion <patrick.ciongoli@gmail.com>
2022-03-13 01:49:00 -0500

    got employeePromt to run after promptManager

--- app.js
+++ app.js
@@ -9,12 +9,13 @@ const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./lib/htmlRenderer");
+const Choice = require("inquirer/lib/objects/choice");
 
 
 // Write code to use inquirer to gather information about the development team members,
 // and to create objects for each team member (using the correct classes as blueprints!)
 
-const promptManager = () => {
+promptManager = () => {
     inquirer
     .prompt([
         { 
@@ -36,151 +37,41 @@ const promptManager = () => {
             type: "input",
             name: "office",
             message: "Please enter the team manager's Office Number (#)."
+        },
+        
+    ])
+    .then((answers) => {
+        console.log(answers);
+        employeePrompt();
+    })
+}
+
+function employeePrompt () {
+    inquirer
+    .prompt ([
+        {
+            type: "list",
+            name: "role",
+            message: "Please select whether you would like to add an employee.",
+            choices: ["Engineer", "Intern", "No. Please finish my profile."]
         }
         
     ])
-
+    .then((answer) => {
+        if (answer.role === "Engineer") {
+            console.log(answer)
+            employeePrompt();
+        } 
+        else if (answer.role === "Intern") {
+            console.log(answer)
+            employeePrompt();
+        } 
+        else {
+            console.log("Your employee portfolio is complete!")
+            return;
+        }
+    })
 }
 
+promptManager();
 
-// const promptEmployee = employeeData => {
-//     if (!employeeData.employee) {
-//         employeeData.employee = [];
-//     }
-
-//     return inquirer 
-//     .prompt([
-//         {
-//             type: "list",
-//             name: "employeeSelect",
-//             message: "Would you like to add another Employee?",
-//             choices: ["Engineer", "Intern", "No. Finish my profile"]
-//         }
-//     ])
-    // .then((selection) => {
-    
-    //     if (selection.employeeSelect === "Engineer") {
-
-    //         inquirer
-    //         .prompt([
-    //             { 
-    //                 type: "input",
-    //                 name: "name",
-    //                 message: "Please enter the team engineer's name."
-    //             },
-    //             {
-    //                 type: "input",
-    //                 name: "id",
-    //                 message: "Please enter the team engineer's id."
-    //             },
-    //             {
-    //                 type: "input",
-    //                 name: "email",
-    //                 message: "Please enter the team engineer's email."
-    //             },
-    //             {
-    //                 type: "input",
-    //                 name: "email",
-    //                 message: "Please enter the team engineer's GitHub username."
-    //             }
-
-    //         ])
-    //     } 
-    // })
-
-    // .then((selection) => {
-    
-    //     if (selection.employeeSelect === "Intern") {
-
-    //         inquirer
-    //         .prompt([
-    //             { 
-    //                 type: "input",
-    //                 name: "name",
-    //                 message: "Please enter the team intern's name."
-    //             },
-    //             {
-    //                 type: "input",
-    //                 name: "id",
-    //                 message: "Please enter the team intern's id."
-    //             },
-    //             {
-    //                 type: "input",
-    //                 name: "email",
-    //                 message: "Please enter the team intern's email."
-    //             },
-    //             {
-    //                 type: "input",
-    //                 name: "email",
-    //                 message: "Please enter the school you team's intern attends."
-    //             }
-
-    //         ])  
-    //     } 
-    // })
-    
-// }
-
-promptManager()
-//   .then(promptEmployee)
-//   .then(employeeData => {
-//       console.log(employeeData);
-
-
-//   });
-
-
-
-
-
-
-
-// After the user has input all employees desired, call the `render` function (required
-// above) and pass in an array containing all employee objects; the `render` function will
-// generate and return a block of HTML including templated divs for each employee!
-
-// After you have your html, you're now ready to create an HTML file using the HTML
-// returned from the `render` function. Now write it to a file named `team.html` in the
-// `output` folder. You can use the variable `outputPath` above target this location.
-// Hint: you may need to check if the `output` folder exists and create it if it
-// does not.
-
-// HINT: each employee type (manager, engineer, or intern) has slightly different
-// information; write your code to ask different questions via inquirer depending on
-// employee type.
-
-// HINT: make sure to build out your classes first! Remember that your Manager, Engineer,
-// and Intern classes should all extend from a class named Employee; see the directions
-// for further information. Be sure to test out each class and verify it generates an
-// object with the correct structure and methods. This structure will be crucial in order
-// for the provided `render` function to work! ```
-
-
-   // promptEngineer() {
-    //     console.log(`
-    // ===================
-    // Add a New Employee
-    // ===================
-    // `);
-
-    // if (!managerData.employee) {
-    //     engineerData.employee = [];
-    // }
-
-  // .then(promptManager => {
-    //     managerData.employee.push(engineerData);
-    //     if (engineerData.confirmAddEngineer) {
-    //         return promptEngineer(engineerData);
-    //     } else {
-    //         return managerData;
-    //     }
-    // })
-
-  // .then(promptEngineer => {
-    //     engineerData.employee.push(internData);
-    //     if (internData.confirmAddIntern) {
-    //         return promptIntern(internData);
-    //     } else {
-    //         return engineerData;
-    //     }
-    // });
